622624807d887d8d165597d292580984f37e3143
serhat-demir <serhatdmr@protonmail.com>
2023-04-16 21:57:39 +0300

    translate design & some changes in intro

---
+++ app/src/main/java/com/serhatd/translate/ui/translate/TranslateFragment.kt
@@ -0,0 +1,15 @@
+package com.serhatd.translate.ui.translate
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.serhatd.translate.R
+
+class TranslateFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_translate, container, false)
+    }
+}
