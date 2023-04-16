33f495b8867206c265d6163e10ed86c9c132b0ad
serhat-demir <serhatdmr@protonmail.com>
2023-04-16 21:59:07 +0300

    define data binding for translate fragment

--- app/src/main/java/com/serhatd/translate/ui/translate/TranslateFragment.kt
+++ app/src/main/java/com/serhatd/translate/ui/translate/TranslateFragment.kt
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.serhatd.translate.R
+import com.serhatd.translate.databinding.FragmentTranslateBinding
 
 class TranslateFragment : Fragment() {
+    private lateinit var binding: FragmentTranslateBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_translate, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_translate, container, false)
+
+        return binding.root
     }
 }
